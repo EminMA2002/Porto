7a514b7334b330423741f7afdd24b1b7a9832fd7
Emin <e.mmmdv02@gmail.com>
2024-09-23 01:30:54 +0400

    v1.0 with js, membership fixed, burger menu fixed.

---
+++ src/js/membership.js
@@ -0,0 +1,43 @@
+let monthly = document.querySelector(".monthly");
+let yearly = document.querySelector(".yearly");
+let packschildmonth = document.querySelector(".packschild1");
+let packschildyear = document.querySelector(".packschild2");
+let blue = document.querySelector(".blue");
+let line3 = document.querySelector(".line3");
+let openedburger = document.querySelector(".openedburger");
+let linex = document.querySelector(".linex");
+let main = document.getElementById("main");
+
+
+
+yearly.addEventListener("click", () => {
+
+    packschildyear.classList.remove("displaynone")
+    packschildmonth.classList.add("displaynone")
+
+    blue.classList.add("swiperight")
+
+});
+
+monthly.addEventListener("click", () => {
+
+    packschildyear.classList.add("displaynone")
+    packschildmonth.classList.remove("displaynone")
+
+    blue.classList.remove ("swiperight")
+
+});
+
+
+line3.addEventListener("click", () => {
+    openedburger.classList.remove("displaynone")
+    main.classList.add("displaynonemain")
+});
+
+
+linex.addEventListener("click", () => {
+    openedburger.classList.add("displaynone")
+    main.classList.remove("displaynonemain")
+});
+
+
