3a888365bd4c11c8723090da8a67e6759009b123
Emin <e.mmmdv02@gmail.com>
2024-09-24 17:24:10 +0400

    v1.3 with js

--- src/js/membership.js
+++ src/js/membership.js
@@ -3,13 +3,6 @@ let yearly = document.querySelector(".yearly");
 let packschildmonth = document.querySelector(".packschild1");
 let packschildyear = document.querySelector(".packschild2");
 let blue = document.querySelector(".blue");
-let line3 = document.querySelector(".line3");
-let openedburger = document.querySelector(".openedburger");
-let linex = document.querySelector(".linex");
-let main = document.getElementById("main");
-let navbuttonburger = document.querySelector(".navbuttonburger");
-
-
 
 yearly.addEventListener("click", () => {
 
@@ -29,19 +22,52 @@ monthly.addEventListener("click", () => {
 });
 
 
+
+let line3 = document.querySelector(".line3");
+let openedburger = document.querySelector(".openedburger");
+let linex = document.querySelector(".linex");
+let main = document.getElementById("main");
+let navbuttonburger = document.querySelector(".navbuttonburger");
+let body = document.querySelector("body")
+
+
 line3.addEventListener("click", () => {
     openedburger.classList.remove("displaynone")
-    main.classList.add("displaynonemain")
 });
 
 
 linex.addEventListener("click", () => {
     openedburger.classList.add("displaynone")
-    main.classList.remove("displaynonemain")
 });
 
 
-navbuttonburger.addEventListener("click", () => {
+window.addEventListener("resize", () => {
     openedburger.classList.add("displaynone")
-    main.classList.remove("displaynonemain")
+});
+
+
+window.addEventListener("scroll", () => {
+    if( openedburger.classList.contains("displaynone") ){
+        body.classList.remove("hidden")
+       
+    }else{ body.classList.add("hidden") }
+});
+
+
+let box = document.querySelector(".box");
+let box1 = document.querySelector(".box1");
+let boxtext = document.querySelector(".boxtext");
+let boxdownarrow = document.querySelector(".boxdownarrow");
+
+
+box.addEventListener("click", ()=>{
+    if(boxtext.classList.contains("displaynone")){
+        boxtext.classList.remove("displaynone")
+        boxdownarrow.classList.add("opacity50")
+        boxdownarrow.classList.add("rotate")
+    }else{
+        boxtext.classList.add("displaynone")
+        boxdownarrow.classList.remove("rotate")
+        boxdownarrow.classList.remove("opacity50")
+    }
 });
